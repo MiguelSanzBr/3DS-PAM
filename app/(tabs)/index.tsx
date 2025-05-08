c7de623a0dba5d06ed9d166852d7720ebc525f4b
Miguel Sanz Boragan <123277580+MiguelSanzBr@users.noreply.github.com>
2025-05-08 12:33:12 +0000

    teste push in vscode

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -28,7 +28,7 @@ export default function HomeScreen() {
           <ThemedText type="defaultSemiBold">
             {Platform.select({
               ios: 'cmd + d',
-              android: 'cmd + m',
+              android: 'cmd + m', 
               web: 'F12',
             })}
           </ThemedText>{' '}
